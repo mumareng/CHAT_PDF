ba64fdad56ea6267c16e53e7ec13b7463488e7da
Muhammad Umar Farooq <mumareng33@gmail.com>
2024-10-24 11:54:32 +0500

    libraries added

--- pdf.py
+++ pdf.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.hello
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+import os
