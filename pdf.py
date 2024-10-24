919906bcee98b70bd466a322b3a9b3a60544607b
Muhammad Umar Farooq <mumareng33@gmail.com>
2024-10-24 12:02:59 +0500

    text updated

--- pdf.py
+++ pdf.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.hello
+st.umar
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
